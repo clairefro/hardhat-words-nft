ca41b33dc4ddb1a31d54bd2751b9f716d259a038
clairefro <claire.froelich@gmail.com>
2021-09-08 22:36:17 -0400

    console.log erroring

---
+++ README.md
@@ -0,0 +1,21 @@
+## Words NFT (with hardhat)
+
+Attempt two, bye truffle
+
+### Dev
+
+`yarn install`
+
+Compile
+`yarn compile`
+
+Test
+`yarn test`
+
+### Helpers
+
+- [OpenZeppelin](https://openzeppelin.com/contracts/) contract template (ERC-721)
+
+### Special thanks to
+
+This tutorial: https://www.freecodecamp.org/news/solidity-tutorial-hardhat-nfts/
--- contracts/Words.sol
+++ contracts/Words.sol
@@ -3,7 +3,10 @@ pragma solidity ^0.7.3;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract Words is ERC721 {
-    constructor(string memory name, string memory symbol)
-        ERC721(name, symbol)
-    {}
+    constructor(string memory name, string memory symbol) ERC721(name, symbol) {
+        // TODO: console log giving undeclared error
+        // console.log("name", name);
+        // console.log("symbol", symbol);
+        // console.log("msg.sender", msg.sender);
+    }
 }
