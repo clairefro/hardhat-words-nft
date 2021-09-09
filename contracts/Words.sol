c93df3129fcd7f3f6dc6a0eafa28ec375bf4daf8
clairefro <claire.froelich@gmail.com>
2021-09-08 21:59:06 -0400

    Add readme

---
+++ contracts/Words.sol
@@ -0,0 +1,9 @@
+pragma solidity ^0.7.3;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract Words is ERC721 {
+    constructor(string memory name, string memory symbol)
+        ERC721(name, symbol)
+    {}
+}
